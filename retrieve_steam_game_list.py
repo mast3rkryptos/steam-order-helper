f29fd967f754f22a799189cfc63ebe1c310bec58
mast3rkryptos <mast3rkryptos@users.noreply.github.com>
2026-06-04 15:40:27 -0500

    Create retrieve_steam_game_list.py

---
+++ retrieve_steam_game_list.py
@@ -0,0 +1,76 @@
+from steam_web_api import Steam
+from decouple import Config, RepositoryEnv
+from datetime import datetime
+
+import csv
+import logging
+import time
+
+# Main Script
+if __name__=="__main__":
+
+    games = []
+
+    # Setup logger
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename='retrieve.log', level=logging.INFO, filemode='w')
+
+    # Begin timer for overall runtime tracker
+    scriptStartTime = time.time()
+    logger.info(f'Started at {datetime.fromtimestamp(scriptStartTime)}')
+
+    # Setup configuration file
+    DOTENV_FILE = '.env'
+    env_config = Config(RepositoryEnv(DOTENV_FILE))
+
+    # Setup Steam connection
+    steam_api_key = env_config.get('STEAM_API_KEY')
+    steam = Steam(steam_api_key)
+    logger.info('Steam API connection initialized.')
+
+    # Retrieve user's steamid
+    steamid = steam.users.search_user('gschive')['player']['steamid']
+    logger.info(f'SteamID: {steamid}')
+
+    # Retrieve user's owned games, and trim results
+    for game in steam.users.get_owned_games(steamid)['games']:
+        games.append([game['appid'], game['name'], game['playtime_forever']])
+        try:
+            achievements = steam.apps.get_user_achievements(steamid, games[-1][0])['playerstats']['achievements']
+            games[-1].append(len([x for x in achievements if x['achieved'] == 1]) / len(achievements))
+        except Exception as e:
+            print('An exception occurred: ', e)
+            games[-1].append(-1)
+        print(games[-1])
+    logger.info(f'Steam Game Count: {len(games)}')
+
+    # Write results to CSV file
+    with open('out.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['appid', 'name', 'playtime_forever', 'achievement'])
+        writer.writerows(games)
+
+    # Print out script execution time
+    scriptFinishTime = time.time()
+    print(f'\nScript Execution Time: {(scriptFinishTime - scriptStartTime):.2f} seconds')
+
+    # Write final log entry
+    logger.info(f'Finished at {datetime.fromtimestamp(scriptFinishTime)} ({(scriptFinishTime - scriptStartTime):.2f} seconds)')
+
+    exit()
+
+    # Begin timer for overall runtime tracker
+    scriptStartTime = time.time()
+
+    # Retrieve Steam game ID list
+    DOTENV_FILE = 'C:\\personal_files\\SteamOrderHelperEnv\\.env'
+    env_config = Config(RepositoryEnv(DOTENV_FILE))
+    KEY = env_config.get("STEAM_API_KEY")
+    steam = Steam(KEY)
+    steamIdList = []
+    for game in steam.users.get_owned_games("76561197990222251")["games"]:
+        print(game)
+
+    # Print out script execution time
+    print(f"\nScript Execution Time: {(time.time() - scriptStartTime):.2f} seconds")
+    exit()
